a18e4c981ea5daa148d170dc4f9e4043e632f122
MoonLordTheSecond <moonlordthesecond@abv.bg>
2026-05-06 18:04:33 +0300

    0.03prot: dR detection
    NEW NAMING 0.00prefix:
    prot prefix = prototype
    fix
    add

--- Thermistor/src/main.cpp
+++ Thermistor/src/main.cpp
@@ -1,31 +1,38 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+const uint8_t 
+  RS = 12 ,
+  E  = 11 ,
+  D4 = 5  ,
+  D5 = 4  ,
+  D6 = 3  ,
+  D7 = 2  ;
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
-const uint8_t RS = 12,
-          Enable = 11,
-              D4 = 5,
-              D5 = 4,
-              D6 = 3,
-              D7 = 2;
-LiquidCrystal lcd(RS, Enable, D4, D5, D6, D7);
+
+
 
 uint8_t thermistorPin = A0;
 
-float thermVal      = 0.00, maxThermVal     = 0.00;
-float thermVoltage  = 0.00, maxThermVoltage = 0.00;
+float value    = 0.00; 
+float maxValue = 0.00;
+float V        = 0.00;
+float Vmax     = 0.00;
 
-uint16_t R     = 0;
-uint16_t minR  = 15000;
+uint16_t R    = 0;
+uint16_t Rmin = 65536;
 
-uint16_t prevR   = 0;
-uint16_t avgR    = 0;
+uint16_t Rprev = 0;
 
-uint32_t sumR    = 0;
-uint32_t counter = 0;
+uint16_t RAvg  = 0;
+uint32_t Rsum  = 0;
+uint16_t R1    = 0;
+uint16_t R2    = 0;
+uint32_t count = 0;
+bool  isFirstCalculation = 1;
 
 uint16_t RdeltaThreshold = 400;
-
-uint16_t SERIESRESISTOR = 10000;
+uint16_t RseriesResistor = 10000;
 /* const uint16_t NOMINAL_R 10000
 const uint8_t NOMINAL_TEMPERATURE 25
 const uint16_t BCOEFFICIENT 3950 */
@@ -49,48 +56,96 @@ char* format_uint16(uint16_t i) {
 
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(thermistorPin, INPUT);
-  lcd.begin(16, 2);
 
-  lcd.setCursor(0, 0); lcd.print("T1 R=");
-  lcd.setCursor(0, 1); lcd.print("T2 R=");
+Serial.begin(9600);
+pinMode(thermistorPin, INPUT);
+lcd.begin(16, 2);
+
+lcd.setCursor(0, 0); lcd.print("T1 R=");
+lcd.setCursor(0, 1); lcd.print("T2 R=");
+
+delay(500); //not sure if will work BUT supposed to wait to NOT have false positive cahnges in R delta
+
 }
 
+
+
+
 void loop() {
-  
-  thermVal = analogRead(thermistorPin);
-  if (thermVal > maxThermVal) maxThermVal = thermVal;
 
-  thermVoltage = thermVal * (5.0 / 1023.0);
-  if (thermVoltage > maxThermVoltage) maxThermVoltage = thermVoltage;
+value = analogRead(thermistorPin);
+if (value > maxValue) maxValue = value;
 
-  R = SERIESRESISTOR * ((1023 / thermVal) - 1);
-  if (R < minR) minR = R;
+V = value * (5.0 / 1023.0);
+if (V > Vmax) Vmax = V;
 
-
-
-  //if(abs(prevR) - abs(R) > RdeltaThreshold) {}
-  
-  sumR += R;
-  counter++;
-  avgR = sumR/counter;
-
-  
-  prevR = R;
+R = RseriesResistor * ((1023 / value) - 1);
+if (R < Rmin) Rmin = R;
 
 
 
 
-  lcd.setCursor(5, 0); lcd.print(format_uint16(avgR));
-  //lcd.setCursor(5, 1); lcd.print(format_uint16());
+if(abs(Rprev - R) > RdeltaThreshold) { // if big change in R - assign averages
 
-  delay(50); // display frame
+  if(count > 0) { // if it's NOT the first read  //prevents divisions by 0  //could increase this 
+    if(isFirstCalculation) {
+      R1 = Rsum/count;
+      isFirstCalculation = false;
+    }
+    else {
+      R2 = Rsum/count;
+      isFirstCalculation = true;
+    }
+  }
+
+  //reset for next stable period 
+  Rsum = R;
+  count = 1;
+
+  Rprev = R;
+}
+
+else { // if stable period - accumulate for averages
+  Rsum += R;
+  count++;
+
+  //RAvg = Rsum/count; //for debugging ONLY - ALL TIME average
+
+  Rprev = R;
+}
 
 
-  Serial.print(thermVal    ); Serial.print(" value"); Serial.print("  "); Serial.print(maxThermVal    ); Serial.print  (" max value"); 
-  Serial.print("      ");
-  Serial.print(thermVoltage); Serial.print(" V"    ); Serial.print("  "); Serial.print(maxThermVoltage); Serial.print  (" max V"    ); 
-  Serial.print("      ");
-  Serial.print(R  ); Serial.print(" ohm"  ); Serial.print("  "); Serial.print(minR  ); Serial.println(" min ohm"  ); 
+
+
+lcd.setCursor(5, 0); lcd.print(format_uint16(R1));
+lcd.setCursor(5, 1); lcd.print(format_uint16(R2));
+delay(50); // display frame
+
+
+
+
+Serial.print(value); Serial.print(" value");
+Serial.print("  ");
+Serial.print(maxValue); Serial.print(" max value"); 
+
+Serial.print("      ");
+
+Serial.print(V); Serial.print(" V"); 
+Serial.print("  "); 
+Serial.print(Vmax); Serial.print(" max V"); 
+
+Serial.print("      ");
+
+Serial.print(R); Serial.print(" ohm");
+Serial.print("  ");
+Serial.print(Rmin); Serial.print(" min ohm"); 
+
+Serial.print("      ");
+
+Serial.print(RAvg); Serial.print(" avg ohm");
+
+
+
+
+Serial.println("");
 }
